e7897b5ece3b55d5f77ea093e864d4e8ecf40581
marcioz98 <marciozgaming@gmail.com>
2018-05-03 22:09:09 +0200

    Initial refactoring

--- main.py
+++ main.py
@@ -1,246 +1,91 @@
-import sys
-import math
 import random
+import math
+import os
 import pygame
 from pygame.locals import *
 
+__version__ = "ALPHA"
+__author__ = "github.com@marcioz98"
 
-class Settings:
-	"""
-	Game Settings controller
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-	It reads from a file and acts as a database
-	of informations in which the game supplies
-	"""
+pygame.init()
 
-	def __init__(self, filename="settings.txt"):
-		self.filename = filename
-		self.resolution = 800, 600
-		self.FPS = 240
-		self.window_parameters = HWACCEL
-		self.game_caption = "Default Game Title"
-		self.read_settings()
-
-	def read_settings(self):
-		with open(self.filename, "r") as input_file:
-
-			for line in input_file:
-				line = line.split(';')
-
-				if line[0] == "resolution" and 'x' in line[1]:
-					resolution = line[1].split('x')
-					self.resolution = int(resolution[0]), int(resolution[1])
-
-				if line[0] == "fps":
-					self.FPS = int(line[1])
-
-				if line[0] == "gamecaption":
-					self.game_caption = line[1][:len(line[1])-1]
-
-				if line[0] == "windowparameters":
-
-					parameters = []
-					accepted_parameters = {
-						"FULLSCREEN" : FULLSCREEN,
-						"HWACCEL" : HWACCEL,
-						"HWSURFACE" : HWSURFACE,
-						"DOUBLEBUF" : DOUBLEBUF
-					}
-
-					for parameter in line[1].replace("\n", "").split("|"):
-
-						if parameter in accepted_parameters:
-							parameters.append(accepted_parameters[parameter])
-
-		for parameter in parameters:
-			self.window_parameters = self.window_parameters | parameter
-
-	def get_resolution(self):
-		return self.resolution
-
-	def get_window_parameters(self):
-		return self.window_parameters
-
-	def get_fps(self):
-		return self.FPS
-
-	def get_game_caption(self):
-		return self.game_caption
+GAME_RES = WIDTH, HEIGHT = 1920, 1080
+FPS = 60
+BG_COLOR = (51, 51, 51)
 
 
-class Lagometer:
-	"""
-	Minecraft's lagometer python similar implementation
+def scale_image(image, factor):
+	return pygame.transform.scale(
+		image,
+		(
+			image.get_rect().width * factor,
+			image.get_rect().height * factor
+		)
+	)
 
-	Simple graph that shows you information about
-	game's ingame performance
-	"""
 
-	def __init__(self):
+def rotate_image_centered(image, rect, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_rect = rotated_image.get_rect(center=rect.center)
+    return rotated_image, rotated_rect
+
+
+class Player(pygame.sprite.Sprite):
+
+	def __init__(self, image, x, y, speed, angle):
+		pygame.sprite.Sprite.__init__(self)
+		self.default_image = image
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.rect.center = self.rect.x, self.rect.y
+		self.speed = speed
+		self.angle = angle
+
+	def move(self):
 		pass
 
-	def draw(self):
-		pass
+	def rotate(self, dangle):
+		self.angle += dangle
+		self.image, self.rect = rotate_image_centered(self.default_image, self.rect, self.angle)
+
+	def set_angle(self, new_angle):
+		self.angle = new_angle
+		self.image, self.rect = rotate_image_centered(self.default_image, self.rect, self.angle)
+
+player_image = pygame.image.load(os.getcwd() + '/sprites/players/player.png')
+player_image = scale_image(player_image, 4)
 
 
-class TimeController:
-	"""
-	Game's Time Controller
+player = Player(player_image, WIDTH//2, HEIGHT//2, 10, 0)
+player_group = pygame.sprite.GroupSingle(player)
 
-	It handles general game timings
-	"""
+window_surface = pygame.display.set_mode(GAME_RES, HWSURFACE|HWACCEL|DOUBLEBUF)
+pygame.display.set_caption(f"PyShooter - Version: {__version__} - {__author__}")
 
-	def __init__(self):
-		self.clock = pygame.time.Clock()
+clock = pygame.time.Clock()
 
+game_ended = False
+while not game_ended:
 
-class GameLogicController:
-	"""
-	Entire Game's Logic Controller
-
-	It handles all the game logic
-	"""
-
-	def __init__(self):
-		self.settings = Settings()
-		self.event_controller = EventController()
-		self.time_controller = TimeController()
-		self.event_controller.set_game_controller(self)
-		self.window = pygame.display
-		try: # If it reads window properties right
-			self.surface = self.window.set_mode(
-				self.settings.get_resolution(),
-				self.settings.get_window_parameters()
-			)
-		except Exception(e): # If screen resolution too big, set default
-			self.surface = self.window.set_mode(
-				(0, 0),
-				self.settings.get_window_parameters()
-			)
-
-		self.window.set_caption(self.settings.get_game_caption())
-		self.FPS = self.settings.get_fps()
-		self.game_ended = False
-
-	def loop(self):
-		"""
-		Main Game Loop
-		"""
-		while not self.game_ended:
-
-			# Event Parsing
-			self.event_controller.parse_events()
-
-			# Game Logic Handling
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			game_ended = True
+		if event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				game_ended = True
 
 
-			# Surface Display Drawing
-			self.surface.fill((125, 125, 125))
+	player.rotate(0.5)
 
-			# Display Updating 
-			self.window.update()
-			self.time_controller.clock.tick(self.FPS)
+	window_surface.fill(BG_COLOR)
 
-			if self.game_ended:
-				self.close_game()
+	player_group.draw(window_surface)
 
-	def end_gameplay(self):
-		self.game_ended = True
+	pygame.display.update()
+	clock.tick(FPS)
 
-	def close_game(self):
-		pygame.quit()
-		exit()
-
-
-class EventController:
-	"""
-	Game's Event Controller
-
-	It handles all the game events
-	"""
-
-	def __init__(self):
-		pass
-
-	def set_game_controller(self, game_controller):
-		self.game_controller = game_controller
-
-	def parse_events(self):
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				self.game_controller.end_gameplay()
-
-			if event.type == KEYDOWN:
-				if event.key == K_F4 or event.key == K_ESCAPE:
-					self.game_controller.end_gameplay()
-
-
-class Enemy:
-	"""
-	Enemy logical representation
-	"""
-
-	def __init__(self):
-		pass
-
-	def draw(self):
-		pass
-
-
-class Player:
-	"""
-	Player logical representation
-	"""
-
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def __calc_player_angle(self):
-		pass
-
-	def __keep_player_under_area(self):
-		pass
-
-	def __check_collosions(self):
-		pass
-
-	def is_dead(self):
-		pass
-
-	def __fire(self):
-		pass
-
-	def draw(self):
-		pass
-
-
-class VFXController:
-	"""
-	Visual Effects Controller
-
-	From this you can decide to draw or not vfxs
-	"""
-
-	def __init__(self):
-		pass
-
-	def draw(self):
-		pass
-
-	def __draw_background_particles(self):
-		pass
-
-	def __draw_player_backfire(self):
-		pass
-
-	def __draw_background_planet(self):
-		pass
-
-	def __draw_shooting_stars(self):
-		pass
-
-
-if __name__ == "__main__":
-	gc = GameLogicController()
-	gc.loop()
+pygame.quit()
